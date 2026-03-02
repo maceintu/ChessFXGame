ec4e448f4bdf7fdd8be04917ffe889c93c83a210
Nicolas <preelnico@gmail.com>
2026-03-02 22:18:26 +0100

    [FIX] setting position into piece

--- src/main/java/fr/perso/chess/backend/general/Cell.java
+++ src/main/java/fr/perso/chess/backend/general/Cell.java
@@ -4,6 +4,8 @@ import fr.perso.chess.backend.pieces.Piece;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
+import java.util.Optional;
+
 public class Cell {
     Cell(Position position){
         this.position = position;
@@ -13,6 +15,10 @@ public class Cell {
     private final ObjectProperty<Piece> pieceProperty = new SimpleObjectProperty<>(null);
     public ObjectProperty<Piece> pieceProperty() { return pieceProperty; }
 
-    public void setPiece(Piece piece) { this.pieceProperty.set(piece); }
+    public void setPiece(Piece piece) {
+        this.pieceProperty.set(piece);
+        if(piece != null)
+            piece.setPosition(this);
+    }
     public Piece getPiece() { return this.pieceProperty.get(); }
 }
