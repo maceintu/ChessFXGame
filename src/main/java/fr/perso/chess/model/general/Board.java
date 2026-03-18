6bfc9bbb4e57edd7a514afedf58773ce940c8d89
Nicolas <preelnico@gmail.com>
2026-03-18 23:58:12 +0100

    [DEV] working progress

--- src/main/java/fr/perso/chess/model/general/Board.java
+++ src/main/java/fr/perso/chess/model/general/Board.java
@@ -2,45 +2,49 @@ package fr.perso.chess.model.general;
 
 import fr.perso.chess.model.pieces.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Board {
-    public Map<Position, Cell> cellMap;
+    public Cell[][] cells;
+    private final List<BoardListener> listeners = new ArrayList<>();
 
     public Board(Player whitePlayer, Player blackPlayer) {
-        this.cellMap = new HashMap<>();
+        cells = new Cell[8][8];
+        for(int row = 0; row<cells.length; row++){
+            for(int col = 0; col<cells.length; col++){
+                cells[row][col] = new Cell(new Position(row, col));
+            }
+        }
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
-                Position pos = new Position(row, col);
-                Cell cell = new Cell(pos);
-                if (row == 1) cell.setPiece(new Pawn(whitePlayer, cell));
-                if (row == 6) cell.setPiece(new Pawn(blackPlayer, cell));
+                if (row == 1) cells[row][col].setPiece(new Pawn(whitePlayer));
+                if (row == 6) cells[row][col].setPiece(new Pawn(blackPlayer));
 
                 if (row == 0 || row == 7) {
                     Player player = (row == 0) ? whitePlayer : blackPlayer;
-                    Piece piece = createHeavyPiece(player, cell, col);
-                    if (piece != null) cell.setPiece(piece);
+                    Piece piece = createHeavyPiece(player, col);
+                    if (piece != null) cells[row][col].setPiece(piece);
                 }
-                this.cellMap.put(pos, cell);
             }
         }
     }
 
-    private Piece createHeavyPiece(Player player, Cell cell, int col) {
+    private Piece createHeavyPiece(Player player, int col) {
         return switch (col) {
-            case 0, 7 -> new Tower(player, cell);
-            case 1, 6 -> new Knight(player, cell);
-            case 2, 5 -> new Bishop(player, cell);
-            case 3 -> new Queen(player, cell);
+            case 0, 7 -> new Tower(player);
+            case 1, 6 -> new Knight(player);
+            case 2, 5 -> new Bishop(player);
+            case 3 -> new Queen(player);
             //case 4    -> new King(player, cell);
-            default -> null;
-        };
-    }
+           default -> null;
+       };
+   }
 
+    public Cell getCell(int row, int col){
+        return cells[row][col];
+    }
     public Cell getCellFromPreviousWithOffset(Cell cell, int offsetRow, int offsetCol) {
-        int currentRow = cell.position.row();
-        int currentCol = cell.position.col();
-        return cellMap.get(new Position(currentRow + offsetRow, currentCol + offsetCol));
+        return cells[cell.position.row() + offsetRow][cell.position.col() + offsetCol];
     }
 }
